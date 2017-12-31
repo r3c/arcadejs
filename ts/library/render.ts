f959feb222e2e0699269e541cae8500fbfe58b31
Remi Caput <git@mirari.fr>
2017-12-31 19:08:12 +0100

    [engine.render] Optimize pixel drawing.

--- ts/library/render.ts
+++ ts/library/render.ts
@@ -1,6 +1,12 @@
 import * as display from "./display";
 import * as math from "./math";
 
+interface Image {
+	data: Uint8ClampedArray;
+	height: number;
+	width: number;
+}
+
 interface Mesh {
 	faces: [number, number, number][];
 	normals?: math.Vector3[];
@@ -11,31 +17,35 @@ const interpolate = (min: number, max: number, ratio: number) => {
 	return min + (max - min) * ratio;
 };
 
-const processScanLine = (context: CanvasRenderingContext2D, y: number, pa: math.Vector3, pb: math.Vector3, pc: math.Vector3, pd: math.Vector3) => {
-	var ratio1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
-	var ratio2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+const blitScanline = (image: Image, y: number, pa: math.Vector3, pb: math.Vector3, pc: math.Vector3, pd: math.Vector3) => {
+	if (y < 0 || y >= image.height)
+		return;
 
-	var x1 = interpolate(pa.x, pb.x, ratio1) >> 0;
-	var x2 = interpolate(pc.x, pd.x, ratio2) >> 0;
+	const offset = y * image.width;
 
-	if (x1 < x2) {
-		for (var x = x1; x < x2; x++) {
-			context.fillStyle = 'white';
-			context.fillRect(x, y, 1, 1);
-		}
-	}
-	else {
-		for (var x = x2; x < x1; x++) {
-			context.fillStyle = 'white';
-			context.fillRect(x, y, 1, 1);
-		}
+	const ratio1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
+	const ratio2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+
+	let x1 = Math.max(Math.min(interpolate(pa.x, pb.x, ratio1) >> 0, image.width - 1), 0);
+	let x2 = Math.max(Math.min(interpolate(pc.x, pd.x, ratio2) >> 0, image.width - 1), 0);
+
+	if (x1 > x2)
+		[x1, x2] = [x2, x1];
+
+	for (var x = x1; x < x2; x++) {
+		const index = (x + offset) * 4;
+
+		image.data[index + 0] = 255;
+		image.data[index + 1] = 255;
+		image.data[index + 2] = 255;
+		image.data[index + 3] = 255;
 	}
 }
 
 /*
 ** From: https://www.davrous.com/2013/06/21/tutorial-part-4-learning-how-to-write-a-3d-software-engine-in-c-ts-or-js-rasterization-z-buffering/
 */
-const drawTriangleFill = (context: CanvasRenderingContext2D, p1: math.Vector3, p2: math.Vector3, p3: math.Vector3) => {
+const blitTriangle = (image: Image, p1: math.Vector3, p2: math.Vector3, p3: math.Vector3) => {
 	// Reorder p1, p2 and p3 so that p1.y <= p2.y <= p3.y
 	if (p1.y > p2.y)
 		[p1, p2] = [p2, p1];
@@ -50,56 +60,32 @@ const drawTriangleFill = (context: CanvasRenderingContext2D, p1: math.Vector3, p
 	const slope12 = p2.y > p1.y ? (p2.x - p1.x) / (p2.y - p1.y) : 0;
 	const slope13 = p3.y > p1.y ? (p3.x - p1.x) / (p3.y - p1.y) : 0;
 
-	// First case where triangles are like that:
-	// P1
-	// -
-	// -- 
-	// - -
-	// -  -
-	// -   - P2
-	// -  -
-	// - -
-	// -
-	// P3
 	if (slope12 > slope13) {
 		for (let y = p1.y; y < p2.y; ++y)
-			processScanLine(context, y, p1, p3, p1, p2);
+			blitScanline(image, y, p1, p3, p1, p2);
 
 		for (let y = p2.y; y <= p3.y; ++y)
-			processScanLine(context, y, p1, p3, p2, p3);
+			blitScanline(image, y, p1, p3, p2, p3);
 	}
 
-	// First case where triangles are like that:
-	//       P1
-	//        -
-	//       -- 
-	//      - -
-	//     -  -
-	// P2 -   - 
-	//     -  -
-	//      - -
-	//        -
-	//       P3
 	else {
 		for (let y = p1.y; y < p2.y; ++y)
-			processScanLine(context, y, p1, p2, p1, p3);
+			blitScanline(image, y, p1, p2, p1, p3);
 
 		for (let y = p2.y; y <= p3.y; ++y)
-			processScanLine(context, y, p2, p3, p1, p3);
+			blitScanline(image, y, p2, p3, p1, p3);
 	}
-};
-
-const drawTriangleWire = (context: CanvasRenderingContext2D, p1: math.Vector3, p2: math.Vector3, p3: math.Vector3) => {
-	context.strokeStyle = 'white';
-	context.beginPath();
-	context.moveTo(p1.x, p1.y);
-	context.lineTo(p2.x, p2.y);
-	context.lineTo(p3.x, p3.y);
-	context.lineTo(p1.x, p1.y);
-	context.stroke();
 };
 
 const draw = (screen: display.Screen, projection: math.Matrix, modelView: math.Matrix, mesh: Mesh) => {
+	const capture = screen.context.getImageData(0, 0, screen.getWidth(), screen.getHeight());
+
+	const image = {
+		data: capture.data,
+		height: screen.getHeight(),
+		width: screen.getWidth()
+	};
+
 	const halfWidth = screen.getWidth() * 0.5;
 	const halfHeight = screen.getHeight() * 0.5;
 
@@ -109,12 +95,14 @@ const draw = (screen: display.Screen, projection: math.Matrix, modelView: math.M
 	const points = mesh.points;
 
 	for (const [i, j, k] of faces) {
-		drawTriangleFill(screen.context,
+		blitTriangle(image,
 			projectToScreen(modelViewProjection, halfWidth, halfHeight, points[i]),
 			projectToScreen(modelViewProjection, halfWidth, halfHeight, points[j]),
 			projectToScreen(modelViewProjection, halfWidth, halfHeight, points[k])
 		);
 	}
+
+	screen.context.putImageData(capture, 0, 0);
 };
 
 const projectToScreen = (modelViewProjection: math.Matrix, halfWidth: number, halfHeight: number, vertex: math.Vector3) => {
