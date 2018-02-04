a6b6d3e34de3f604ad1533253a8378d69f85e831
Remi Caput <git@mirari.fr>
2018-02-04 10:29:50 +0100

    [scene.s03][scene.s04]Rename scenes s03 and s04.

--- ts/main.ts
+++ ts/main.ts
@@ -2,8 +2,8 @@ import * as application from "./engine/application";
 import * as s00 from "./scene/s00_blank";
 import * as s01 from "./scene/s01_projection";
 import * as s02 from "./scene/s02_rotation";
-import * as s03 from "./scene/s03_transform";
-import * as s04 from "./scene/s04_rasterize";
+import * as s03 from "./scene/s03_composition";
+import * as s04 from "./scene/s04_rasterization";
 import * as s05 from "./scene/s05_webgl";
 import * as s06 from "./scene/s06_lightning";
 
@@ -11,8 +11,8 @@ application.initialize([
 	application.declare("s00: blank", s00.scenario),
 	application.declare("s01: projection", s01.scenario),
 	application.declare("s02: rotation", s02.scenario),
-	application.declare("s03: transform", s03.scenario),
-	application.declare("s04: rasterize", s04.scenario),
+	application.declare("s03: composition", s03.scenario),
+	application.declare("s04: rasterization", s04.scenario),
 	application.declare("s05: webgl", s05.scenario),
 	application.declare("s06: lightning", s06.scenario)
 ]);
