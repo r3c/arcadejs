09b54fdfb6c7d38bcc36fb05d0b67db10b66a1a1
Remi Caput <git@mirari.fr>
2018-02-17 16:43:00 +0100

    [scene.s06] Internal renaming.

--- ts/scene/s06-forward-lightning.ts
+++ ts/scene/s06-forward-lightning.ts
@@ -1,6 +1,7 @@
 import * as application from "../engine/application";
 import * as controller from "../engine/controller";
 import * as display from "../engine/display";
+import * as functional from "../engine/language/functional";
 import * as io from "../engine/io";
 import * as matrix from "../engine/math/matrix";
 import * as model from "../engine/graphic/model";
@@ -25,22 +26,26 @@ interface Configuration {
 	useHeightMap: boolean
 }
 
+interface Light {
+	position: vector.Vector3
+}
+
 interface CallState {
-	lightPositions: vector.Vector3[],
+	lightPositions: Light[],
 	projectionMatrix: matrix.Matrix4,
 	tweak: application.Tweak<Configuration>,
 	viewMatrix: matrix.Matrix4
 }
 
 interface SceneState {
-	bulbs: vector.Vector3[],
 	camera: view.Camera,
 	gl: WebGLRenderingContext,
 	input: controller.Input,
+	lights: Light[],
 	models: {
-		bulb: webgl.Model,
 		cube: webgl.Model,
-		ground: webgl.Model
+		ground: webgl.Model,
+		light: webgl.Model
 	},
 	move: number,
 	projectionMatrix: matrix.Matrix4,
@@ -54,7 +59,7 @@ interface SceneState {
 
 const configuration = {
 	nbLights: ["0", ".1", "2", "3"],
-	animate: true,
+	animate: false,
 	useAmbient: true,
 	useDiffuse: false,
 	useSpecular: false,
@@ -112,28 +117,28 @@ const prepare = async (tweak: application.Tweak<Configuration>) => {
 	lightShader.bindPerMaterialProperty("specularColor", gl => gl.uniform4fv, state => state.material.specularColor);
 	lightShader.bindPerMaterialTexture("specularMap", state => state.material.specularMap);
 
-	const bulbs = [0, 1, 2].map(i => ({ x: 0, y: 0, z: 0 }));
+	const lights = functional.range(50, i => ({ position: { x: 0, y: 0, z: 0 } }));
 
 	for (const index of [0, 1, 2]) {
 		lightShader.bindPerCallProperty("light" + index + ".enabled", gl => gl.uniform1i, state => index < state.tweak.nbLights ? 1 : 0);
-		lightShader.bindPerCallProperty("light" + index + ".position", gl => gl.uniform3fv, state => [state.lightPositions[index].x, state.lightPositions[index].y, state.lightPositions[index].z]);
+		lightShader.bindPerCallProperty("light" + index + ".position", gl => gl.uniform3fv, state => [state.lightPositions[index].position.x, state.lightPositions[index].position.y, state.lightPositions[index].position.z]);
 	}
 
 	// Load models
-	const bulbModel = await model.fromOBJ("./res/model/sphere.obj", { transform: matrix.Matrix4.createIdentity().scale({ x: 0.2, y: 0.2, z: 0.2 }) });
 	const cubeModel = await model.fromJSON("./res/model/cube.json");
 	const groundModel = await model.fromJSON("./res/model/ground.json");
+	const lightModel = await model.fromOBJ("./res/model/sphere.obj", { transform: matrix.Matrix4.createIdentity().scale({ x: 0.2, y: 0.2, z: 0.2 }) });
 
 	// Create state
 	return {
-		bulbs: bulbs,
 		camera: new view.Camera({ x: 0, y: 0, z: -5 }, { x: 0, y: 0, z: 0 }),
 		gl: gl,
 		input: runtime.input,
+		lights: lights,
 		models: {
-			bulb: webgl.loadModel(gl, bulbModel),
 			cube: webgl.loadModel(gl, cubeModel),
-			ground: webgl.loadModel(gl, groundModel)
+			ground: webgl.loadModel(gl, groundModel),
+			light: webgl.loadModel(gl, lightModel)
 		},
 		move: 0,
 		projectionMatrix: matrix.Matrix4.createPerspective(45, runtime.screen.getRatio(), 0.1, 100),
@@ -160,9 +165,9 @@ const render = (state: SceneState) => {
 		.rotate({ x: 0, y: 1, z: 0 }, camera.rotation.y);
 
 	// Draw scene
-	const bulbs = state.bulbs.slice(0, state.tweak.nbLights).map(bulb => ({
-		matrix: matrix.Matrix4.createIdentity().translate(bulb),
-		model: models.bulb
+	const lights = state.lights.slice(0, state.tweak.nbLights).map(light => ({
+		matrix: matrix.Matrix4.createIdentity().translate(light.position),
+		model: models.light
 	}));
 
 	const cube = {
@@ -176,7 +181,7 @@ const render = (state: SceneState) => {
 	};
 
 	const callState = {
-		lightPositions: state.bulbs,
+		lightPositions: state.lights,
 		projectionMatrix: state.projectionMatrix,
 		tweak: state.tweak,
 		viewMatrix: cameraView
@@ -188,21 +193,21 @@ const render = (state: SceneState) => {
 	gl.cullFace(gl.BACK);
 
 	target.clear();
-	target.draw(shaders.basic, bulbs, callState);
+	target.draw(shaders.basic, lights, callState);
 	target.draw(shaders.light, [cube, ground], callState);
 };
 
 const update = (state: SceneState, dt: number) => {
-	// Update light bulb positions
+	// Update light positions
 	if (state.tweak.animate) {
 		state.move += dt * 0.00003;
 	}
 
-	for (let i = 0; i < state.bulbs.length; ++i) {
+	for (let i = 0; i < state.lights.length; ++i) {
 		const pitch = state.move * (((i + 1) * 17) % 23);
 		const yaw = state.move * (((i + 1) * 7) % 13);
 
-		state.bulbs[i] = vector.Vector3.scale({
+		state.lights[i].position = vector.Vector3.scale({
 			x: Math.cos(yaw) * Math.cos(pitch),
 			y: Math.sin(yaw) * Math.cos(pitch),
 			z: Math.sin(pitch)
