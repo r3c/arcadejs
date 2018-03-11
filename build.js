c7d49738bd6d497cb31c1549e9d977fc1953ade7
Remi Caput <git@mirari.fr>
2018-03-11 23:03:02 +0100

    Add support for "watch" command separated from "build" one.

--- build.js
+++ build.js
@@ -1,28 +1,51 @@
+#!/usr/bin/env node
+'use strict';
+
+const argparse = require("argparse");
 const browserify = require("browserify");
 const fs = require("fs");
 const tsify = require("tsify");
 const watchify = require("watchify");
 
-const browserifyOptions = {
+// Read command line arguments
+const parser = new argparse.ArgumentParser({
+	addHelp:		true,
+	description:	'ArcadeJS build CLI.',
+	version:		'0.0.1'
+});
+
+parser.addArgument(['-w', '--watch'], {
+	action:	'storeTrue',
+	help:	'Enable watch mode (rebuild on file change).'
+});
+
+const args = parser.parseArgs();
+
+// Configure browserify & tsify
+const project = browserify({
 	cache: {},
 	debug: true,
-	entries: ['src/main.ts'],
+	entries: ["src/main.ts"],
 	packageCache: {}
+})
+	.plugin("tsify");
+
+// Create bundle from current project
+const bundle = function () {
+	const stream = project.bundle();
+
+	stream
+		.on("error", console.error)
+		.pipe(fs.createWriteStream(__dirname + "/www/js/bundle.js"));
 };
 
-const project = browserify(browserifyOptions)
-	.plugin("tsify")
-	.plugin(watchify);
-
-const bundle = function () {
+// Enable watchify mode if requested
+if (args.watch) {
 	project
-		.bundle()
-		.on('error', console.error)
-		.pipe(fs.createWriteStream(__dirname + "/www/js/bundle.js"));
+		.plugin(watchify)
+		.on("log", console.log)
+		.on("update", bundle);
 }
 
-project
-	.on('log', console.log)
-	.on('update', bundle);
-
+// Initiate first bundle
 bundle();
--- serve.js
+++ serve.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict';
+
 const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
