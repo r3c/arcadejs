838657e4ee3e2c5c9451c06707d3892a5af2f285
Remi Caput <git@mirari.fr>
2018-03-25 15:51:27 +0200

    [scene.shared] Clamp generated color components.

--- src/scene/shared/color.ts
+++ src/scene/shared/color.ts
@@ -1,12 +1,13 @@
 
 const createBright = (index: number) => {
+	const y = 0.8;
 	const u = ((index * 1.17) % 2 - 1) * 0.436;
 	const v = ((index * 1.43) % 2 - 1) * 0.615;
 
 	return {
-		x: 1.0 + 1.13983 * v,
-		y: 1.0 - 0.39465 * u - 0.5806 * v,
-		z: 1.0 + 2.03211 * u
+		x: Math.min(y + 1.13983 * v, 1),
+		y: Math.min(y - 0.39465 * u - 0.5806 * v, 1),
+		z: Math.min(y + 2.03211 * u, 1)
 	};
 };
 
