be543331e78d02e1d82afa1d7259f7a63f7179ea
Remi Caput <git@mirari.fr>
2018-03-25 14:20:58 +0200

    [engine.webgl] Implement directional lights in deferred shading renderer.

--- src/scene/shared/move.ts
+++ src/scene/shared/move.ts
@@ -1,11 +1,11 @@
 import * as vector from "../../engine/math/vector";
 
-const rotate = (index: number, amount: number, width: number, height: number) => {
+const orbitate = (index: number, amount: number, width: number, height: number) => {
 	const offset = index + 1;
 
 	const distance = Math.cos(((offset * 31) % 71) / 71 + amount * ((offset * 37) % 73) / 73 * 0.001);
-	const pitch = (offset * 11 % 41) / 41 * 2 * Math.PI + amount * ((offset * 17) % 47) / 47;
-	const yaw = (offset * 23 % 59) / 59 * 2 * Math.PI + amount * ((offset * 29) % 67) / 67;
+	const pitch = ((offset * 11) % 41) / 41 * 2 * Math.PI + amount * ((offset * 17) % 47) / 47;
+	const yaw = ((offset * 23) % 59) / 59 * 2 * Math.PI + amount * ((offset * 29) % 67) / 67;
 
 	return {
 		x: Math.cos(yaw) * Math.cos(pitch) * distance * width,
@@ -14,4 +14,15 @@ const rotate = (index: number, amount: number, width: number, height: number) =>
 	};
 };
 
-export { rotate }
+const rotate = (index: number, amount: number) => {
+	const offset = index + 1;
+	const angle = (offset * 11) % 41 + amount * ((offset * 17) % 47) / 47;
+
+	return {
+		x: Math.cos(angle),
+		y: Math.PI * 1 / 6,
+		z: Math.sin(angle)
+	};
+};
+
+export { orbitate, rotate }
