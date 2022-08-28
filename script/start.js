ceb350e7240cc5bb4c67109c9c656fb05113a6d9
Remi Caput <v.mirari.fr+git@mirari.fr>
2022-08-28 18:17:11 +0200

    [script] Fix "start" command.

--- script/start.js
+++ script/start.js
@@ -6,7 +6,7 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/../dist"));
 app.listen(port);
 
 console.log(`server started on http://localhost:${port}/`);
