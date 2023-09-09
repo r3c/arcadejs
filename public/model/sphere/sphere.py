d8e01b19b0b721b91e50feca2e6326b86df57c64
Remi Caput <v.mirari.fr+git@mirari.fr>
2023-09-09 17:47:53 +0200

    [public.model] Fix sphere generation script.

--- public/model/sphere/sphere.py
+++ public/model/sphere/sphere.py
@@ -1,8 +1,92 @@
 #! python
 
+import itertools
 import json
 import math
 import sys
+
+
+def sphere(cx, cy, cz, slices, stacks, radius):
+    polygon = {
+        "coordinates": [],
+        "indices": [],
+        "materialName": "default",
+        "normals": [],
+        "positions": []
+    }
+
+    # Write vertices - Top
+    polygon["coordinates"].append({"u": 0.5, "v": 0})
+    polygon["normals"].append({"x": 0, "y": 1, "z": 0})
+    polygon["positions"].append({"x": cx, "y": cy + radius, "z": cz})
+
+    # Write vertices - Middle
+    for i in range(0, stacks - 1):
+        a = (i + 1) * math.pi / stacks
+
+        for j in range(0, slices + 1):
+            b = j * math.pi * 2 / slices
+
+            x = math.cos(b) * math.sin(a)
+            y = math.cos(a)
+            z = math.sin(b) * math.sin(a)
+
+            polygon["coordinates"].append({
+                "u": float(j) / slices,
+                "v": float(i + 1) / stacks
+            })
+            polygon["normals"].append({"x": x, "y": y, "z": z})
+            polygon["positions"].append({
+                "x": cx + x * radius,
+                "y": cy + y * radius,
+                "z": cz + z * radius
+            })
+
+    # Write vertices - Bottom
+    polygon["coordinates"].append({"u": 0.5, "v": 1})
+    polygon["normals"].append({"x": 0, "y": -1, "z": 0})
+    polygon["positions"].append({"x": cx, "y": cy - radius, "z": cz})
+
+    # Write indices - Top triangles
+    fixed = 0
+    shift = 1
+
+    for i in range(0, slices):
+        polygon["indices"].append({
+            "x": shift + i + 1,
+            "y": shift + i,
+            "z": fixed
+        })
+
+    # Write indices - Quad strips
+    for i in range(0, stacks - 2):
+        for j in range(0, slices):
+            shift = 1 + i * (slices + 1)
+
+            polygon["indices"].append({
+                "x": shift + j,
+                "y": shift + j + 1,
+                "z": shift + j + slices + 1
+            })
+            polygon["indices"].append({
+                "x": shift + j + slices + 1,
+                "y": shift + j + 1,
+                "z": shift + j + slices + 2
+            })
+
+    # Write indices - Bottom triangles
+    fixed = 1 + (stacks - 1) * (slices + 1)
+    shift = 1 + (stacks - 2) * (slices + 1)
+
+    for i in range(0, slices):
+        polygon["indices"].append({
+            "x": fixed,
+            "y": shift + i,
+            "z": shift + i + 1
+        })
+
+    return polygon
+
 
 try:
     if len(sys.argv) >= 4:
@@ -21,76 +105,6 @@ if slices < 2 or stacks < 2:
     sys.stderr.write("error: slices and stacks must be >= 2\n")
     sys.exit(1)
 
-mesh = {
-    "coordinates": [],
-    "materialName": "default",
-    "normals": [],
-    "positions": [],
-    "triangles": []
-}
-
-# Write vertices - Top
-mesh["coordinates"].append({"u": 0.5, "v": 0})
-mesh["normals"].append({"x": 0, "y": 1, "z": 0})
-mesh["positions"].append({"x": 0, "y": radius, "z": 0})
-
-# Write vertices - Middle
-for i in range(0, stacks - 1):
-    a = (i + 1) * math.pi / stacks
-
-    for j in range(0, slices + 1):
-        b = j * math.pi * 2 / slices
-
-        x = math.cos(b) * math.sin(a)
-        y = math.cos(a)
-        z = math.sin(b) * math.sin(a)
-
-        mesh["coordinates"].append({
-            "u": float(j) / slices,
-            "v": float(i + 1) / stacks
-        })
-        mesh["normals"].append({"x": x, "y": y, "z": z})
-        mesh["positions"].append({
-            "x": x * radius,
-            "y": y * radius,
-            "z": z * radius
-        })
-
-# Write vertices - Bottom
-mesh["coordinates"].append({"u": 0.5, "v": 1})
-mesh["normals"].append({"x": 0, "y": -1, "z": 0})
-mesh["positions"].append({"x": 0, "y": -radius, "z": 0})
-
-# Write indices - Top triangles
-fixed = 0
-shift = 1
-
-for i in range(0, slices):
-    mesh["indices"].append({x: shift + i + 1, y: shift + i, z: fixed})
-
-# Write indices - Quad strips
-for i in range(0, stacks - 2):
-    for j in range(0, slices):
-        shift = 1 + i * (slices + 1)
-
-        mesh["indices"].append({
-            x: shift + j,
-            y: shift + j + 1,
-            z: shift + j + slices + 1
-        })
-        mesh["indices"].append({
-            x: shift + j + slices + 1,
-            y: shift + j + 1,
-            z: shift + j + slices + 2
-        })
-
-# Write indices - Bottom triangles
-fixed = 1 + (stacks - 1) * (slices + 1)
-shift = 1 + (stacks - 2) * (slices + 1)
-
-for i in range(0, slices):
-    mesh["indices"].append({x: fixed, y: shift + i, z: shift + i + 1})
-
 # Write as JSON
 json.dump(
     {
@@ -103,7 +117,10 @@ json.dump(
                 "shininess": 100
             }
         },
-        "meshes": [mesh]
+        "polygons": [
+            sphere(x, y, z, slices, stacks, radius)
+            for x, y, z in itertools.product([0], [0], [0])
+        ]
     },
     sys.stdout,
     indent=True,
