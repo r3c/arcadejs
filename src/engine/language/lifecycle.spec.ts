f25d740bcd6ab6a4146366cd31d0c926953c4f53
Remi Caput <v.mirari.fr+git@mirari.fr>
2025-08-28 23:36:53 +0200

    [engine.graphic] Implement subject removal in binding painter.

--- src/engine/language/lifecycle.spec.ts
+++ src/engine/language/lifecycle.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it, vi } from "vitest";
 import { createDelegateDisposable } from "./lifecycle";
 
 describe("createDelegateDisposable", () => {
-  it("should delegate call", () => {
+  it("should dispose registered objects", () => {
     const disposable = createDelegateDisposable();
     const mock1 = vi.fn();
     const mock2 = vi.fn();
@@ -12,6 +12,22 @@ describe("createDelegateDisposable", () => {
     disposable.dispose();
 
     expect(mock1).toHaveBeenCalledOnce();
+    expect(mock2).toHaveBeenCalledOnce();
+  });
+
+  it("should not dispose removed objects", () => {
+    const disposable = createDelegateDisposable();
+    const mock1 = vi.fn();
+    const mock2 = vi.fn();
+    const disposable1 = { dispose: mock1 };
+    const disposable2 = { dispose: mock2 };
+
+    disposable.register(disposable1);
+    disposable.register(disposable2);
+    disposable.remove(disposable1);
+    disposable.dispose();
+
+    expect(mock1).not.toHaveBeenCalled();
     expect(mock2).toHaveBeenCalledOnce();
   });
 });
