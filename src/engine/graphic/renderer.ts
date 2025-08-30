910bdb9df513afbaf2a412724aad104c13273e63
Remi Caput <979446+r3c@users.noreply.github.com>
2025-08-30 16:40:56 +0200

    [engine.graphic] New `Renderer` forward lighting implementation. (#50)

---
+++ src/engine/graphic/renderer.ts
@@ -0,0 +1,20 @@
+import { Renderer, RendererSubject } from "./renderer/definition";
+import {
+  createForwardLightingRenderer,
+  ForwardLightingConfiguration,
+  ForwardLightingLightModel,
+  ForwardLightingRenderer,
+  ForwardLightingScene,
+  ForwardLightingSubject,
+} from "./renderer/forward-lighting";
+
+export {
+  type ForwardLightingConfiguration,
+  type ForwardLightingRenderer,
+  type ForwardLightingScene,
+  type ForwardLightingSubject,
+  type Renderer,
+  type RendererSubject,
+  ForwardLightingLightModel,
+  createForwardLightingRenderer,
+};
